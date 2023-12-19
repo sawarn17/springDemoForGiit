fff71b8445e0b1183e4a101d36d211d0bd9977bb
sawarn17 <ayamshifo@gmail.com>
2023-12-19 12:13:00 +0530

    mysql setup done

---
+++ src/main/java/com/ayam/ayam/config/DataSourceConfig.java
@@ -0,0 +1,16 @@
+//package com.ayam.ayam.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DataSourceConfig{
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSourceBuilder.url("localhost:33");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("root");
+//        return dataSourceBuilder.build();
+//    }
+//}
